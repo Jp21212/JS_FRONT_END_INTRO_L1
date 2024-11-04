015f68e6a0f6717a5cfcddc69d92b104f45ccf28
Jp21212 <Jamesp2814@tuckahoeschools.org>
2024-11-04 13:32:46 +0000

    Initial commit

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function changeTextColor(){
 });
 }
 changeTextColor();
+
+const pictureElement = document.getElementById("#toggle-image")
+function changePicture(){
+const images = [
+   "https://www.pbs.org/wnet/nature/files/2014/10/Monkey-Main-1280x720.jpg"
+   "https://media.npr.org/assets/img/2015/09/23/ap_836720500193-d90a20e2b8d735f74d436f36054eb3dc2bd96696.jpg?s=1100&c=85&f=jpeg"
+   "https://i.natgeofe.com/n/82fddbcc-4cbb-4373-bf72-dbc30068be60/drill-monkey-01.jpg?w=2560&h=1920"
+   "https://kidszoo.org/wp-content/uploads/2023/09/swamp-monkey_intern-alyssa-3-scaled.jpg"
+
+];
+
+let currentIndex = 0;
+
+pictureElement.addEventListener("click", () =>{
+    currentIndex = (currentIndex + 1) % images.length;
+    pictureElement.src = images[currentIndex]
+})
+}
+changePicture()
